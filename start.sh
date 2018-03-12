b78c3a6331d97bc94caee710818d301be1145abc
Adrian Amaglio <Vous@exemple.com>
2018-03-12 14:57:49 +0100

    start

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+upload_dir=/uploads
+web_user=www-data
+
+location=`dirname $0`
+upload_dir_link=$location/uploads
+
+
 die() { echo >&2 -e "\nERROR: $@\n"; exit 1; }
 run() { "$@"; code=$?; [ $code -ne 0 ] && die "command [$*] failed with error code $code"; }
 
@@ -8,3 +15,8 @@ if [ $UID != 0 ] ; then
 fi
 
 apt install clamav-daemon
+mkdir -p $upload_dir
+ln -s $upload_dir $upload_dir_link
+chown -R $web_user:$web_user .
+run nginx -c $nginx_conf_file
+
